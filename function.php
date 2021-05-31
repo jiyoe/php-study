84ecf468568e62e2183ee8b7b203b5093471a085
jiyoe <ejdgodl@naver.com>
2021-05-31 18:54:43 +0900

    php function

---
+++ function.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Function</title>
+</head>
+<body>
+    <h1>Function</h1>
+    <?php 
+    $str = "Write a short pitch about
+    what you do and why
+    I help you create a home you love, so
+    you can relax and refresh in a pure
+    and simple space.";
+    echo $str;
+    ?>
+    </body>
+</html> 
